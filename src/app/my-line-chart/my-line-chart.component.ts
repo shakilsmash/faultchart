2688e6baa85a8c9c9a8d337eb316ec0a8cabe2ee
shakilsmash <smash.shakil@gmail.com>
2019-02-19 00:44:31 +0600

    ToolTip Done

--- src/app/my-line-chart/my-line-chart.component.ts
+++ src/app/my-line-chart/my-line-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PluginHoverline } from './plugin-hoverline';
  
 @Component({
   selector: 'line-chart-demo',
@@ -12,16 +13,117 @@ export class MyLineChartComponent {
   public lineChartLabels:Array<any> = ['12AM', '1AM', , , '4AM', '5AM', '6AM'];
   public lineChartOptions:any = {
     responsive: true,
-    scales: { yAxes: [{ display: false }] }
+    scales: { yAxes: [{ display: false }] },
+    lineOnHover: {
+      enabled: true,
+      //lineColor: '#bbb',
+      lineColor: 'white',
+      lineWidth: 1
+   },
+   hover: {
+    mode: 'nearest',
+    intersect: true
+  },
+  tooltips: {
+    mode: 'index',
+    intersect: false,
+    enabled: false,
+  //   callbacks: {
+  //     label: function(tooltipItem, data) {
+  //         var label = '     <h2>Start:&#013;&#010;' + ['new line'+ 'another line'];
+  //         if (label) {
+  //             label += ':: ';
+  //         }
+  //         label += Math.round(tooltipItem.yLabel * 100) / 100 + '</h2>';
+  //         return label;
+  //     }
+    
+  // }
+  custom: function(tooltipModel) {
+    // Tooltip Element
+    var tooltipEl = document.getElementById('chartjs-tooltip');
+
+    // Create element on first render
+    if (!tooltipEl) {
+        tooltipEl = document.createElement('div');
+        tooltipEl.id = 'chartjs-tooltip';
+        tooltipEl.innerHTML = "<table></table>";
+        document.body.appendChild(tooltipEl);
+    }
+
+    // Hide if no tooltip
+    if (tooltipModel.opacity === 0) {
+        tooltipEl.style.opacity = '0';
+        return;
+    }
+
+    // Set caret Position
+    tooltipEl.classList.remove('above', 'below', 'no-transform');
+    if (tooltipModel.yAlign) {
+        tooltipEl.classList.add(tooltipModel.yAlign);
+    } else {
+        tooltipEl.classList.add('no-transform');
+    }
+
+    function getBody(bodyItem) {
+        return bodyItem.lines;
+    }
+
+    // Set Text
+    if (tooltipModel.body) {
+        var titleLines = tooltipModel.title || [];
+        var bodyLines = tooltipModel.body.map(getBody);
+
+        var innerHtml = '<tbody>' +
+                            '<tr><td>Start:</td><td bgcolor="#bababa"> Sun 1 Jul 2018 18:26:18 GMD+8</td></tr>' + 
+                            '<tr><td>End:</td><td> Sun 1 Jul 2018 18:32:03 GMD+8</td></tr>' + 
+                            '<tr><td>Length:</td><td>00:05:45</td></tr>' + 
+                            '<tr><td>Keywords:</td><td>works, yay, best</td></tr>'
+                        ;
+        
+        // bodyLines.forEach(function(body, i) {
+        //     var colors = tooltipModel.labelColors[i];
+        //     var style = 'background:' + colors.backgroundColor;
+        //     style += '; border-color:' + colors.borderColor;
+        //     style += '; border-width: 2px';
+        //     var span = '<span style="' + style + '"></span>';
+        //     innerHtml += '<tr><td>' + span + body + '</td></tr>';
+        // });
+        innerHtml += '</tbody>';
+
+        var tableRoot = tooltipEl.querySelector('table');
+        tableRoot.innerHTML = innerHtml;
+    }
+
+    // `this` will be the overall tooltip
+    var position = this._chart.canvas.getBoundingClientRect();
+
+    // Display, position, and set styles for font
+    tooltipEl.style.opacity = '1';
+    tooltipEl.style.position = 'absolute';
+    tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+    tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+    tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+    tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+    tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+    tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+    tooltipEl.style.pointerEvents = 'none';
+}
+  }
   };
   public lineChartColors:Array<any> = [
     { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
+      // backgroundColor: 'rgba(148,159,177,0.2)',
+      backgroundColor: 'rgba(255,255,255,0.2)',
       borderColor: 'rgba(148,159,177,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      // pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+      //below are new
+      pointRadius: 5,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'white',
     }
   ];
   public lineChartLegend:boolean = true;
